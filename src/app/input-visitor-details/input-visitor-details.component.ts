1f6814a536c0bb2ef285918f7a5b8f4a4749a166
Andua Julian Giodani <andua.julian@gmail.com>
2023-07-06 01:37:54 +0700

    update: -list visitor

--- src/app/input-visitor-details/input-visitor-details.component.ts
+++ src/app/input-visitor-details/input-visitor-details.component.ts
@@ -6,42 +6,40 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-input-visitor-details',
   templateUrl: './input-visitor-details.component.html',
-  styleUrls: ['./input-visitor-details.component.css']
+  styleUrls: ['./input-visitor-details.component.css'],
 })
 export class InputVisitorDetailsComponent implements OnInit {
-  @Input() visitor:any;
+  @Input() visitor: any;
   visitorSubs?: Subscription;
 
-  constructor(private visitorService : VisitorService, private router: Router) {
+  constructor(private visitorService: VisitorService, private router: Router) {
     this.visitor = {
-    name:"",
-    email:"",
-    gender:"",
-    address:"",
-    nohp:"",
-    queue:""
+      name: '',
+      email: '',
+      gender: '',
+      address: '',
+      nohp: '',
+      queue: '',
+    };
   }
-}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   insertData(): void {
     this.visitorSubs = this.visitorService
       .insert(this.visitor)
       .subscribe((result) => {
-        if(result){
-          this.router.navigateByUrl('/')
+        if (result) {
+          this.router.navigateByUrl('/');
         }
       });
   }
 
-  clickBtn(){
-    this.insertData()
+  clickBtn(): void {
+    this.insertData();
   }
 
   ngOnDestroy(): void {
     this.visitorSubs?.unsubscribe();
   }
-
 }
--- src/app/visitor-list/visitor-list.component.ts
+++ src/app/visitor-list/visitor-list.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { VisitorService } from 'src/app/service/visitor.service';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-visitor-list',
   templateUrl: './visitor-list.component.html',
-  styleUrls: ['./visitor-list.component.css']
+  styleUrls: ['./visitor-list.component.css'],
 })
 export class VisitorListComponent implements OnInit {
+  allData: any;
 
-  constructor() { }
+  visitorSubs?: Subscription;
+
+  constructor(private router: Router, private visitorService: VisitorService) {}
 
   ngOnInit(): void {
+    this.getAll();
   }
 
+  getAll(): void {
+    this.visitorSubs = this.visitorService.getAll().subscribe((result) => {
+      this.allData = result;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.visitorSubs?.unsubscribe();
+  }
 }
